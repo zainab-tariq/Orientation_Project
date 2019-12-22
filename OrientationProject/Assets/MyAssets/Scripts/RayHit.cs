4b3ba553a64f56af9c275091b046ff4fd00078cf
Zainab Tariq <zainabtariq12@gmail.com>
2019-12-22 18:21:13 +0100

    line Renderer lasers added

---
+++ OrientationProject/Assets/MyAssets/Scripts/RayHit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayHit : MonoBehaviour
+{
+    LineRenderer laserLine;
+
+    [SerializeField] GameObject child;
+    public float minRadialDistance = 1;
+    public float maxRadialDistance = 25;
+
+    void start() {
+        laserLine = gameObject.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        laserLine = gameObject.GetComponent<LineRenderer>();
+        RaycastHit hitInfo;
+        laserLine.SetPosition(0, transform.position);
+
+        Physics.Linecast(transform.position,
+                        transform.position + transform.forward * maxRadialDistance,
+                         out hitInfo);
+
+        if (hitInfo.collider)
+        {
+            laserLine.SetPosition(1, hitInfo.point);
+            child.SetActive(true);
+            child.transform.position = hitInfo.point; //new Vector3(rhit.point.x, rhit.point.y, transform.position.z);
+        }
+        else
+        {
+            //laserLine.SetPosition(1, transform.position + transform.forward * 100.0f);
+            child.SetActive(false);
+        }
+    }
+}
