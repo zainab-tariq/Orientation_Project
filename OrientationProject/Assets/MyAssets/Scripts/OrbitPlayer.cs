fd968f3eb7522ba2db5fefa053a9866edd68c13a
Zainab Tariq <zainabtariq12@gmail.com>
2020-02-12 15:44:27 +0100

    add more laser and spheres + add orbitPlayer script

---
+++ OrientationProject/Assets/MyAssets/Scripts/OrbitPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitPlayer : MonoBehaviour
+{
+    // Use this for initialization
+
+    public Transform target;
+    public float orbitDistance = 2.0f;
+    public float orbitDegreesPerSec = 180.0f;
+    public Vector3 relativeDistance = Vector3.zero;
+    public bool once = true;
+    // Use this for initialization
+    void Start()
+    {
+        //Orbit at start 
+        if (target != null)
+        {
+            relativeDistance = transform.position - target.position;
+        }
+    }
+
+    void Orbit()
+    {
+
+        // to do: orbitDistance random between two vales
+        // relativeDistance A-axis random between 2 values 
+        // set orbit speed
+
+        if (target != null)
+        {
+            // Keep us at the last known relative position
+            transform.position = (target.position + relativeDistance);
+            transform.RotateAround(target.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+            // Reset relative position after rotate
+            if (once)
+            {
+                transform.position *= orbitDistance;
+                once = false;
+            }
+            relativeDistance = transform.position - target.position;
+        }
+    }
+
+    void LateUpdate()
+    {
+
+        Orbit();
+
+    }
+}
