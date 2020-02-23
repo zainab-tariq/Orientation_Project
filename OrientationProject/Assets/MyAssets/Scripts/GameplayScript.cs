2abf6b863febbee1f2e9568f5bfc137218d81bbd
Zainab Tariq <zainabtariq12@gmail.com>
2020-02-23 13:42:42 +0100

    script for changing teleportation color

--- OrientationProject/Assets/MyAssets/Scripts/GameplayScript.cs
+++ OrientationProject/Assets/MyAssets/Scripts/GameplayScript.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+namespace Valve.VR.InteractionSystem
+{
 public class GameplayScript : MonoBehaviour
 {
     [SerializeField]
-    private Material teleportAreaHighlight;
-    [SerializeField]
-    private Material currentColorMaterial;
-    [SerializeField]
-    private Material teleportArcMaterial;
+    public Teleport Teleporting;
+    public Material currentColorMaterial;
+    //[SerializeField]
+    //private Material teleportArcMaterial;
     Color currrentColor;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        currrentColor = currentColorMaterial.GetColor("TintColor");
-    }
+        void Start()
+        {
+            currrentColor = currentColorMaterial.GetColor("_TintColor");
+        }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        teleportAreaHighlight.SetColor("tintColor", currrentColor);
-        teleportArcMaterial.SetColor("tintColor", currrentColor);
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            Teleporting.areaHighlightedMaterial.SetColor("_TintColor", currrentColor);
+            Teleporting.pointerValidColor = currrentColor;
+            //teleportArcMaterial.SetColor("tintColor", currrentColor); 
+        }
     }
 }
