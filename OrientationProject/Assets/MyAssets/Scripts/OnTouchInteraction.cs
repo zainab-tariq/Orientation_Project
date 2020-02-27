c3295195780971d356b71eeeed6ac291da9accb1
Zainab Tariq <zainabtariq12@gmail.com>
2020-02-27 12:46:38 +0100

    script update

--- OrientationProject/Assets/MyAssets/Scripts/OnTouchInteraction.cs
+++ OrientationProject/Assets/MyAssets/Scripts/OnTouchInteraction.cs
@@ -4,129 +4,91 @@ using System.Collections;
 
 namespace Valve.VR.InteractionSystem.Sample
 {
-        //-------------------------------------------------------------------------
-        [RequireComponent(typeof(Interactable))]
-        public class OnTouchInteraction : MonoBehaviour
-        {
-            private Vector3 oldPosition;
-            private Quaternion oldRotation;
+    //-------------------------------------------------------------------------
+    [RequireComponent(typeof(Interactable))]
+    public class OnTouchInteraction : MonoBehaviour
+    {
+        private Interactable interactable;
+        private AudioSource _audio;
 
-            private float attachTime;
+        [Header("Animation")]
+        public GameObject AnimatedGameObject;
 
-            private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+        [SerializeField]
+        public string AnimationStateName;
+        Animator anim;
 
-            private Interactable interactable;
-            private AudioSource _audio;
-            [Header("Animation")]
-            public GameObject AnimatedGameObject;
+        [Header("Interactable Sphere")]
+        private Vector3 target; //taget position for translation in y-axis
+        public float TargetYPos = 24;
+        [SerializeField]
+        private float speed = 1;
+        [SerializeField]
+        private bool disappearOnHover = false;
 
-            [SerializeField]
-            public string AnimationStateName;
-            Animator anim;
-            [Header("Interactable Sphere")]
-            public float TargetYPos = 24;
-            [SerializeField]
-            private float speed = 1;
-            private Vector3 target;
         //-------------------------------------------------
         void Awake()
-            {
-                anim = AnimatedGameObject.GetComponent<Animator>();
-                interactable = this.GetComponent<Interactable>();
-                _audio = this.GetComponent<AudioSource>();
-                target = new Vector3(this.transform.position.x, TargetYPos, this.transform.position.z);
+        {
+            anim = AnimatedGameObject.GetComponent<Animator>();
+            interactable = this.GetComponent<Interactable>();
+            _audio = this.GetComponent<AudioSource>();
+            target = new Vector3(this.transform.position.x, TargetYPos, this.transform.position.z);
+        }
+
+        //-------------------------------------------------
+        // Called when a Hand starts hovering over this object
+        //-------------------------------------------------
+        private void OnHandHoverBegin(Hand hand)
+        {
+            if(this.gameObject.GetComponent<Animator>() != null){
+                this.gameObject.GetComponent<Animator>().enabled = false;
             }
 
-            //-------------------------------------------------
-            // Called when a Hand starts hovering over this object
-            //-------------------------------------------------
-            private void OnHandHoverBegin(Hand hand)
+            if (this.gameObject.tag == "Sphere" && disappearOnHover)
             {
-                this.gameObject.GetComponent<Animator>().enabled = false;
-                if(this.gameObject.tag == "StartSphere"){
-                        anim.Play(AnimationStateName);
-                        this.GetComponent<MeshRenderer>().enabled = false;
-                        this.transform.position = new Vector3(this.transform.position.x, TargetYPos, this.transform.position.z); // set y üosition to a fixed position
-                        //transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed); //move towards a fixed y postion  
+                if (anim != null)
+                {
+                    anim.Play(AnimationStateName);
                 }
-
+                this.GetComponent<MeshRenderer>().enabled = false;
+                this.transform.position = new Vector3(this.transform.position.x, TargetYPos, this.transform.position.z); // set y üosition to a fixed position
+                //transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed); //move towards a fixed y postion  
                 _audio.Play();
                 //StartCoroutine("waitTillAudioFinish"); //deactivate gameobject once the audio is played
             }
+        }
 
-            IEnumerator waitTillAudioFinish(){
-                yield return new WaitForSeconds(_audio.clip.length);
-                this.gameObject.SetActive(false);
-            }
+        IEnumerator waitTillAudioFinish()
+        {
+            yield return new WaitForSeconds(_audio.clip.length);
+            this.gameObject.SetActive(false);
+        }
 
-            //-------------------------------------------------
-            // Called when a Hand stops hovering over this object
-            //-------------------------------------------------
-            private void OnHandHoverEnd(Hand hand)
+        //-------------------------------------------------
+        // Called every Update() while a Hand is hovering over this object
+        //-------------------------------------------------
+        private void HandHoverUpdate(Hand hand)
+        {
+            GrabTypes startingGrabType = hand.GetGrabStarting();
+            if (startingGrabType != GrabTypes.None && this.gameObject.tag == "Sphere")
             {
-       
-            }
-
-
-            //-------------------------------------------------
-            // Called every Update() while a Hand is hovering over this object
-            //-------------------------------------------------
-            private void HandHoverUpdate(Hand hand)
-            {
-               
-            }
-
-
-            //-------------------------------------------------
-            // Called when this GameObject becomes attached to the hand
-            //-------------------------------------------------
-            private void OnAttachedToHand(Hand hand)
-            {
-                
-            }
-
-
-
-            //-------------------------------------------------
-            // Called when this GameObject is detached from the hand
-            //-------------------------------------------------
-            private void OnDetachedFromHand(Hand hand)
-            {
-                
-            }
-
-
-            //-------------------------------------------------
-            // Called every Update() while this GameObject is attached to the hand
-            //-------------------------------------------------
-            private void HandAttachedUpdate(Hand hand)
-            {
-                
-            }
-
-            private bool lastHovering = false;
-            private void Update()
-            {
-                if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
+                if (anim != null)
                 {
-                    lastHovering = interactable.isHovering;
+                    anim.Play(AnimationStateName);
                 }
-            }
-
-
-            //-------------------------------------------------
-            // Called when this attached GameObject becomes the primary attached object
-            //-------------------------------------------------
-            private void OnHandFocusAcquired(Hand hand)
-            {
-            }
-
-
-            //-------------------------------------------------
-            // Called when another attached GameObject becomes the primary attached object
-            //-------------------------------------------------
-            private void OnHandFocusLost(Hand hand)
-            {
+                this.GetComponent<MeshRenderer>().enabled = false;
+                this.transform.position = new Vector3(this.transform.position.x, TargetYPos, this.transform.position.z);
+                _audio.Play();
             }
         }
+
+        private bool lastHovering = false;
+        private void Update()
+        {
+            if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
+            {
+                lastHovering = interactable.isHovering;
+            }
+        }
+    }
 }
