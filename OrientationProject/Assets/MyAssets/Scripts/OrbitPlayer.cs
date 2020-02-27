73b6b1ffb2e205d7d988c79b2ea7320e8c80cc60
Zainab Tariq <zainabtariq12@gmail.com>
2020-02-27 14:31:14 +0100

    update orbit script

--- OrientationProject/Assets/MyAssets/Scripts/OrbitPlayer.cs
+++ OrientationProject/Assets/MyAssets/Scripts/OrbitPlayer.cs
@@ -4,48 +4,45 @@ using UnityEngine;
 
 public class OrbitPlayer : MonoBehaviour
 {
-    // Use this for initialization
-
     public Transform target;
+    public float sphereTranslateSpeed = 1.0f;
     public float orbitDistance = 2.0f;
     public float orbitDegreesPerSec = 180.0f;
     public Vector3 relativeDistance = Vector3.zero;
-    public bool once = true;
-    // Use this for initialization
+    public float yPosition = 0;
+
     void Start()
     {
-        //Orbit at start 
-        if (target != null)
+         if (target != null)
         {
-            relativeDistance = transform.position - target.position;
+            //// to do translate the spehre to the orbit position 
+            //// example: transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * sphereTranslateSpeed);
+            // orbit around target gameObject 
+            //relativeDistance = transform.position - target.position;
+            relativeDistance = new Vector3(transform.position.x - target.position.x, yPosition, transform.position.z - target.position.z);
+            
         }
+        
     }
 
     void Orbit()
     {
-
-        // to do: orbitDistance random between two vales
-        // relativeDistance A-axis random between 2 values 
-        // set orbit speed
-
         if (target != null)
         {
             // Keep us at the last known relative position
             transform.position = (target.position + relativeDistance);
+            //rotate
             transform.RotateAround(target.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
-            // Reset relative position after rotate
-            if (once)
-            {
-                transform.position *= orbitDistance;
-                once = false;
-            }
-            relativeDistance = transform.position - target.position;
+            //reset relative position after rotate
+            relativeDistance = new Vector3(transform.position.x - target.position.x, yPosition, transform.position.z - target.position.z);
+            //relativeDistance = transform.position - target.position;
+            //gradualy move with the player and still orbit
+            transform.position = Vector3.MoveTowards(transform.position, relativeDistance, Time.deltaTime * sphereTranslateSpeed);
         }
     }
 
     void LateUpdate()
     {
-
         Orbit();
 
     }
