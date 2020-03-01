e004e57016875d6c3ab2e7182a625d0687bbdc0f
Zainab Tariq <zainabtariq12@gmail.com>
2020-03-01 16:50:17 +0100

    orbit script updated

--- OrientationProject/Assets/MyAssets/Scripts/OrbitPlayer.cs
+++ OrientationProject/Assets/MyAssets/Scripts/OrbitPlayer.cs
@@ -8,51 +8,36 @@ public class OrbitPlayer : MonoBehaviour
     public float sphereTranslateSpeed = 1.0f;
     public float orbitDistance = 2.0f;
     public float orbitDegreesPerSec = 180.0f;
-    public Vector3 relativeDistance = Vector3.zero;
+    private Vector3 relativeDistance = Vector3.zero;
+    private Vector3 playerPosition;
     public float yPosition = 0;
 
-    void Start()
-    {
-        if (target != null)
-        {
-            //// to do translate the spehre to the orbit position 
-            //// example: transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * sphereTranslateSpeed);
-            // orbit around target gameObject 
-            //relativeDistance = transform.position - target.position;
-            relativeDistance = new Vector3(transform.position.x - target.position.x, yPosition, transform.position.z - target.position.z);
-
-        }
-
-    }
 
     void Orbit()
     {
-        if (target != null)
-        {
-            // Keep us at the last known relative position
-            transform.position = (target.position + relativeDistance);
-            //rotate
-            transform.RotateAround(target.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
-            //reset relative position after rotate
-            relativeDistance = new Vector3(transform.position.x - target.position.x, yPosition, transform.position.z - target.position.z);
-            //relativeDistance = transform.position - target.position;
-            //gradualy move with the player and still orbit
-            transform.position = Vector3.MoveTowards(transform.position, relativeDistance, Time.deltaTime * sphereTranslateSpeed);
-        }
+        if(target != null)
+            {
+                Vector3 targetPosition = new Vector3(target.position.x, yPosition, target.position.z);
+                Vector3 transformPosition = new Vector3(transform.position.x, yPosition, transform.position.z);
+                // Keep us at orbitDistance from target
+                transform.position = targetPosition + (transformPosition - targetPosition).normalized * orbitDistance;
+                transform.RotateAround(target.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+            }
     }
 
     void LateUpdate()
     {
-            Orbit();
-            /*
-        if (transform.position == relativeDistance)
+            //Orbit();
+            
+        if (transform.position.z >= target.position.z - orbitDistance)
         {
             Orbit();
         }
         else
         {
-            //transform.position = Vector3.MoveTowards(transform.position, relativeDistance, Time.deltaTime * sphereTranslateSpeed);
+            playerPosition = new Vector3(target.position.x , yPosition, target.position.z + orbitDistance); // current player position update in case player moves
+            transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime * sphereTranslateSpeed);
         }
-        */
+        
     }
 }
