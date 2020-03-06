f35c3e513c92c8a15364720ce137fd65f3fdb1c3
Zainab Tariq <zainabtariq12@gmail.com>
2020-03-06 15:10:26 +0100

    gip input script

---
+++ OrientationProject/Assets/MyAssets/Scripts/GripInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GripInput : MonoBehaviour
+{
+
+    private float fadeDuration = 1f;
+    public SteamVR_Input_Sources handType; // 1
+    public SteamVR_Action_Boolean grabAction; // 3
+    public bool GetGrab() // 2
+    {
+        return grabAction.GetState(handType);
+    }
+
+    void Update()
+    {
+        if (GetGrab())
+        {
+            print("Grab " + handType);
+            //FadeToBlack();
+            //StartCoroutine("waitAndEndGame");
+        }
+    }
+     IEnumerator waitAndEndGame()
+        {
+             //wait for fade to finish
+            yield return new WaitForSeconds(fadeDuration + 3);
+            Application.Quit();
+            //UnityEditor.EditorApplication.isPlaying = false;
+            
+        }
+     private void FadeToBlack()
+        {
+            //set start color
+            SteamVR_Fade.Start(Color.clear, 0f);
+            //set and start fade to
+            SteamVR_Fade.Start(Color.black, fadeDuration);
+        }
+}
